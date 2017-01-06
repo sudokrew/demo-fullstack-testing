127e8fa3cd6c5921beea3c53b913f65648095855
Tony Gaskell <thgaskell@gmail.com>
2017-01-06 02:22:36 -1000

    Disable database logging for test environment

--- config/config.js
+++ config/config.js
@@ -2,9 +2,12 @@ const path = require('path');
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
+const logging = NODE_ENV != 'test' ? console.log : false;
+
 const config = {
   storage: path.resolve(__dirname, '..', 'data', `${NODE_ENV}.sqlite`),
   dialect: 'sqlite',
+  logging,
 };
 
 module.exports = config;
